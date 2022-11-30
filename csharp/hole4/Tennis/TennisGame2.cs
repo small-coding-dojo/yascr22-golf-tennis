7d7719d90a867fa7a91cb77d3c5c3de006a18c41
projector-rider-4 <yascr22@example.com>
2022-11-30 13:19:04 +0000

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:csharp/hole4/Tennis/TennisGame2.cs

--- csharp/hole4/Tennis/TennisGame2.cs
+++ csharp/hole4/Tennis/TennisGame2.cs
@@ -21,47 +21,47 @@ namespace Tennis
         {
             var score = "";
             if (p1point == p2point && p1point < 3)
-                score = ScoreAsString(p1point) + "-All";
+                return ScoreAsString(p1point) + "-All";
             if (p1point == p2point && p1point > 2)
-                score = "Deuce";
+                return "Deuce";
 
             if ((p1point > 0 && p2point == 0) || (p2point > 0 && p1point == 0))
             {
                 p1res = ScoreAsString(p1point);
                 p2res = ScoreAsString(p2point);
-                score = p1res + "-" + p2res;
+                return p1res + "-" + p2res;
             }
 
             if (p1point > p2point && p1point < 4)
             {
                 p1res = ScoreAsString(p1point);
                 p2res = ScoreAsString(p2point);
-                score = p1res + "-" + p2res;
+                return p1res + "-" + p2res;
             }
             if (p2point > p1point && p2point < 4)
             {
                 p2res = ScoreAsString(p2point);
                 p1res = ScoreAsString(p1point);
-                score = p1res + "-" + p2res;
+                return p1res + "-" + p2res;
             }
 
             if (p1point > p2point && p2point >= 3)
             {
-                score = "Advantage player1";
+                return "Advantage player1";
             }
 
             if (p2point > p1point && p1point >= 3)
             {
-                score = "Advantage player2";
+                return "Advantage player2";
             }
 
-            if (p1point >= 4 && p2point >= 0 && (p1point - p2point) >= 2)
+            if (p1point >= 4 && (p1point - p2point) >= 2)
             {
-                score = "Win for player1";
+                return "Win for player1";
             }
-            if (p2point >= 4 && p1point >= 0 && (p2point - p1point) >= 2)
+            if (p2point >= 4 && (p2point - p1point) >= 2)
             {
-                score = "Win for player2";
+                return "Win for player2";
             }
             return score;
         }
